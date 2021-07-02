500d6d4dd6239f167a7ca47b4bdddef57f8ee6a5
yatin-07 <nish07ravenclaw@gmail.com>
2021-07-02 22:05:55 +0530

    images updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import React from 'react';
+import HomePage from './pages/homepage/hompage.component.jsx';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello world
+    <div>
+      <HomePage />
     </div>
   );
 }
